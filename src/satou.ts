5711788f91ddafacff9d205bf73911dc8f449233
Circle <1176092962@qq.com>
2022-02-01 16:52:38 +0800

    add answers

--- src/satou.ts
+++ src/satou.ts
@@ -1,11 +1,15 @@
 import { Context } from 'koishi'
+import { sample } from 'lodash'
 
 export function apply(ctx: Context) {
+
+  const yesList = ['爱你', '爱你❤️', '❤️❤️❤️❤️❤️']
+  const noList = ['艹', 'bka']
   
   ctx.middleware(async (session, next) => {
     const message = session.content.trim()
     if (message === '砂糖爱我吗') {
-      return session.userId === '1016607834' ? '爱你' : 'bka'
+      return session.userId === '1016607834' ? sample(yesList) : sample(noList)
     } else {
       return next()
     }
