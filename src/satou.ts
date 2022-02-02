f61cff27d18f0bfa26c5c20ee1e0bcba6ac89f05
Circle <1176092962@qq.com>
2022-02-02 20:40:16 +0800

    update satou

--- src/satou.ts
+++ src/satou.ts
@@ -2,15 +2,16 @@ import { Context } from 'koishi'
 import { sample } from 'lodash'
 
 export function apply(ctx: Context) {
-  console.log(ctx.app.http.config)
 
-  const yesList = ['爱你', '爱你❤️', '❤️❤️❤️❤️❤️❤️']
+  const yesList = ['爱你', '爱你❤️', '❤️❤️❤️❤️❤️❤️', '敲喜翻']
   const noList = ['艹', 'bka']
   
   ctx.middleware(async (session, next) => {
     const message = session.content.trim()
     if (message === '砂糖爱我吗') {
       return session.userId === '1016607834' ? sample(yesList) : sample(noList)
+    } else if (message === '砂糖可爱吗') {
+      return sample(['ka', '可爱', 'bka', '不可爱'])
     } else {
       return next()
     }
